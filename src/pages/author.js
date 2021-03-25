2a1c3cc5a0d3f615e00afe1a2d54b475c3333790
foxnacity <foxnacity@gmail.com>
2021-03-25 15:29:50 +0100

    adding more styles

---
+++ src/pages/author.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from '../components/head'
+import Layout from '../components/layout'
+
+const about = () => {
+    return (
+        <Layout>
+            <Head title="O autorze" />
+            <h2>
+                Hello there
+            </h2>
+        </Layout>
+    )
+}
+
+export default about
---
+++ src/posts/react/react.md
@@ -0,0 +1,15 @@
+---
+title: "React"
+date: "16.03.2021"
+---
+
+Zabawa z plikami md
+REAAACT
+
+![grafika](./testowezdj.png)
+
+## Tytuł
+
+1. punkt
+2. punkt1
+3. punkt2
